b6d23880d6c75c1c485cb3fdf31578753787337a
M.Aldi Gunawan <aldigunawan1808@gmail.com>
2024-08-31 21:47:33 +0700

    Optimizing Logic
    reducing public object by combining it into one methods

--- js/script.js
+++ js/script.js
@@ -1,11 +1,4 @@
 const GameBoard = (() => {
-  let board = Array.from({length: 3}, () => Array(3).fill(null));
-
-  let rounds = 0;
-  let turns;
-
-  let currentMark = '';
-
   const players = {
     player1: {
       marker: '',
@@ -19,25 +12,16 @@ const GameBoard = (() => {
     }
   }
 
-  const setMark = () => {
-    if(currentMark === players['player1'].marker){
-      currentMark = players['player2'].marker;
-    }else if(currentMark === players['player2'].marker){
-      currentMark = players['player1'].marker;
-    }else {
-      alert(`not your turn ${currentMark}`)
-    }
-  }
+  let board = Array.from({length: 3}, () => Array(3).fill(null));
+
+  let rounds = 1;
+  let currentPlayer = 'player1';
+  let round = true;
   
   const gameLogic = (x, y, mark) => {
     let horizontal = true;
     let vertical = true;
     let diagonal = false;
-    let tie = false;
-
-    if(isItTie()){
-      tie = true;
-    }
     
     let highlight = {};
 
@@ -54,21 +38,17 @@ const GameBoard = (() => {
         }
       }
 
-      // I think need optimizations
       // Check Diagonal
-      if(x === 0 && y === 0 || x === 1 && y === 1 || x === 2 && y === 2 || x === 0 && y === 2 || x === 2 && y === 0){
-        if(
-          board[x][y] === board[1][1] &&
-          board[x][y] === board [2][2] &&
-          board[x][y] === board[0][0])
-        {
-          diagonal = true;
-        }else if(
-          board[x][y] === board[1][1] &&
-          board[x][y] === board [2][0] &&
-          board[x][y] === board[0][2])
-        {
-          diagonal = true;
+      const isMainDiagonal = (x === y);
+      const isAntiDiagonal = (x + y === 2);
+
+      if(isMainDiagonal || isAntiDiagonal) {
+        if(isMainDiagonal) {
+          diagonal = board[0][0] === mark && board[1][1] === mark && board[2][2] === mark;
+        }
+
+        if(isAntiDiagonal){
+          diagonal = board[0][2] === mark && board[1][1] === mark && board[2][0] === mark;
         }
       }
     }
@@ -85,16 +65,15 @@ const GameBoard = (() => {
       console.log(board);
       
       // Adding Scores;
-      const currentPlayer = getPlayerByMarker();
       players[currentPlayer].scores++;
 
       return true;
-    }else if(tie){
-      console.log('Tie');
-      return true;
     }
-
     return false;
+  }
+
+  const switchPlayer = () => {
+    currentPlayer = currentPlayer === 'player1' ? 'player2' : 'player1';
   }
 
   const isItTie = () => {
@@ -106,19 +85,15 @@ const GameBoard = (() => {
       }
     }
 
+    console.log('Tie');
     return true;
   }
 
-  const getPlayerByMarker = () => {
-    console.log(currentMark);
-    for (const playerKey in players) {
-      if (players[playerKey].marker === currentMark) {
-        return playerKey;
-      }
-    }
+  const reset = () => {
+    board = Array.from({length: 3}, () => Array(3).fill(null));
+    round = true;
+    currentPlayer = 'player1';
   }
-
-  let round = true;
   
   const roundEnd = () => {
     console.log("Round Ended");
@@ -127,6 +102,50 @@ const GameBoard = (() => {
       player2: players['player2'].scores
     });
     round = false;
+    rounds++;
+
+    if(rounds > 3){
+      gameEnded();
+    }
+  }
+
+  const gameEnded = () => {
+    let winner;
+    let player;
+
+    if(players.player1.scores === players.player2.scores) {
+      console.log("It's a Tie");
+      return;
+    }else {
+      if(players.player1.scores > players.player2.scores) {
+        winner = players.player1;
+        player = 1;
+      }else{
+        winner = players.player2;
+        player = 2;
+      }
+    }
+
+    if(winner.human) {
+      console.log(`Player ${player} Win`)
+    }else{
+      console.log('AI Win')
+    }
+  }
+
+  const manageRound = (restartGame = false) => {
+    if(restartGame) {
+      rounds = 1;
+      players.player1.scores = 0;
+      players.player2.scores = 0;
+    }else{
+      if(rounds > 3) {
+        console.log('Game Ended');
+        return;
+      }
+    }
+    
+    reset();
   }
   
   return {
@@ -140,77 +159,71 @@ const GameBoard = (() => {
         players[player].human = isHuman;
       }
 
-      currentMark = players['player1'].marker;
       console.log(players);
     },
-    getMark: () => {
-      return currentMark;
-    },
-    getTurn: () => {
-      const currentPlayer = getPlayerByMarker();
-      return players[currentPlayer].human;
-    },
-    playerMoves: (x, y) => {
-      if(board[x][y] === null){
-        board[x][y] = currentMark;
+    playerInfo: () => ({
+      marker: players[currentPlayer].marker,
+      isHuman: players[currentPlayer].human
+    }),
+    move: (x, y) => {
+      if(board[x][y] === null && round){
+        board[x][y] = players[currentPlayer].marker;
+
+        if(gameLogic(x, y, board[x][y]) || isItTie()){
+          roundEnd();
+        }else{
+          switchPlayer();
+        }
       }else{
-        console.log('filled');
-        return;
+        console.log('filled or round ended');
+        return true;
       }
-
-      if(gameLogic(x, y, currentMark)){
-        // Reset Game
-        roundEnd();
-        return;
-      }
-
-      setMark();
     },
-    round : () => {
-      return round;
-    },
-    getScores: () => {
-      return {
+    getScores: () => ({
         player1: players['player1'].scores,
         player2: players['player2'].scores
-      }
-    },
-    reset: () => {
-      board = Array.from({length: 3}, () => Array(3).fill(null));
-      round = true;
-    }
+    }),
+    manageRound
   };
 })();
 
-GameBoard.player('player1', '#', true);
-GameBoard.player('player2', '*', false);
-
 const move = (x, y) => {
-  if(GameBoard.round()){
-    console.log(x, y)
-    console.log(GameBoard.getTurn());
-    if(GameBoard.getTurn()){
-      GameBoard.playerMoves(x, y);
-      GameBoard.getBoard();
-    }else{
-      GameBoard.playerMoves(x, y);
-      GameBoard.getBoard();
-    }
+  console.log(x, y)
+  console.log(GameBoard.playerInfo().isHuman);
+  if(GameBoard.playerInfo().isHuman){
+    GameBoard.move(x, y);
+    GameBoard.getBoard();
   }else{
-    console.log("Round Ended");
+    aiMoves(x, y);
+    GameBoard.getBoard();
   }
 }
 
-const reset = () => {
-  GameBoard.reset();
+const aiMoves = (x, y) => {
+  while(!GameBoard.move(x, y)){
+    x = Math.floor(Math.random() * 3);
+    y = Math.floor(Math.random() * 3);
+    console.log(x, y)
+    GameBoard.getBoard();
+  }
+}
+
+const next = (restart = false) => {
+  GameBoard.manageRound(restart);
 }
 
 document.addEventListener('click', (e) => {
   if(e.target.closest('.circle')){
-    const x = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
-    const y = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+    const x = Math.floor(Math.random() * 3);
+    const y = Math.floor(Math.random() * 3);
     move(x, y);
   }else if(e.target.closest('.reset')){
-    reset();
+    next();
+  }else if(e.target.closest('.restart')){
+    next(true);
   }
 })
+
+
+GameBoard.player('player1', '#', true);
+GameBoard.player('player2', '*', false);
